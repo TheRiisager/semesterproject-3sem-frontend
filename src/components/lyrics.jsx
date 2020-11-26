6887ca7eb398bc20d14d99b5b7ccc38b5b067cc9
Lasse-Emil Støvring Larsen <lforlasse@gmail.com>
2020-11-26 12:09:40 +0100

    endnu et forsøg på at ramme keys under render

--- src/components/lyrics.jsx
+++ src/components/lyrics.jsx
@@ -1,41 +1,46 @@
 import { useEffect, useState } from 'react';
 
 fetch(" https://orion.apiseeds.com/api/music/lyric/Eminem/Lose yourself"
-// + props.artist + "/" + props.songName
+  // + props.artist + "/" + props.songName
   + "?apikey=gcNODnOj5VuEJKCD98kixEwzNsNTVujeAfBregVPgARsL6tP2uCGSRZZnXXdluGt")
-    .then(res => res.json())
-    .then(data => console.log(data)
-    )
+  .then(res => res.json())
+  .then(data => console.log(data)
+  )
 
 
-function LyricsText (){
+function LyricsText() {
 
 }
 
 function LyricsConnect(props) {
-    let [serverData, setServerData] = useState({})
-    useEffect(() => {
-        fetch(" https://orion.apiseeds.com/api/music/lyric/Eminem/Lose yourself"
-        // + props.artist + "/" + props.songName
-          + "?apikey=gcNODnOj5VuEJKCD98kixEwzNsNTVujeAfBregVPgARsL6tP2uCGSRZZnXXdluGt")
-            .then(res => res.json())
-            .then(data => setServerData(data)
-            )
-    },)
+  let serverDataMap =
+    [{ key: "result.track.text", value:""},
+    { key: "result.artist.name", value:"" },
+    { key: "result.track.name", value:"" }];
+
+  let [serverData, setServerData] = useState({serverDataMap})
+useEffect(() => {
+  fetch(" https://orion.apiseeds.com/api/music/lyric/Eminem/Lose yourself"
+    // + props.artist + "/" + props.songName
+    + "?apikey=gcNODnOj5VuEJKCD98kixEwzNsNTVujeAfBregVPgARsL6tP2uCGSRZZnXXdluGt")
+    .then(res => res.json())
+    .then(data => setServerData(data)
+    )
+}, [])
 
 
-    return (
-        <div>
-            <p>Du har ramt lyrics</p>
-            <label htmlFor="songName">Song name</label>
-            <input disabled id="songName" value="Lose yourself"></input>
-            <label htmlFor="artistName">Artist</label>
-            <input disabled id="arstistName" value=""></input>
-            <br></br>
-            <textarea readOnly value=""></textarea>
-            <p>{serverData.result.track.text}</p>
-        </div>
-    );
+return (
+  <div>
+    <p>Du har ramt lyrics</p>
+    <label htmlFor="songName">Song name</label>
+    <input disabled id="songName" value="Lose yourself"></input>
+    <label htmlFor="artistName">Artist</label>
+    <input disabled id="arstistName" value=""></input>
+    <br></br>
+    <textarea readOnly value=""></textarea>
+    <p>{serverData.result.artist.name}</p>
+  </div>
+);
 }
 
 export default LyricsConnect;
